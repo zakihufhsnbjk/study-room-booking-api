bdb10a300077b587b466a2345e2bb8b4251d09f3
Zak Ahmed <zakahmed@Zaks-MacBook-Pro.local>
2026-05-18 17:34:34 +0200

    Add Swagger UI

--- src/main/java/com/example/studybooking/security/SecurityConfig.java
+++ src/main/java/com/example/studybooking/security/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
